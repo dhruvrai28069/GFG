16b75efd708021e346d287dc9ba27ee77bc91e79
Dhruv <dhruvrai28069@gmail.com>
2026-02-25 23:19:27 +0530

    Search in an almost Sorted Array

---
+++ Search_in_an_almost_Sorted_Array.java
@@ -0,0 +1,30 @@
+public class Search_in_an_almost_Sorted_Array {
+
+    public int findTarget(int arr[], int target) {
+
+        int n = arr.length;
+        int low = 0;
+        int high = n-1;
+
+        while(low <= high){
+
+            int mid = low + (high - low)/2;
+            if(target == arr[mid]){
+                return mid;
+            }
+            if(mid > 0 && target == arr[mid-1]){
+                return mid-1;
+            }
+            if(mid < n-1 && target == arr[mid+1]){
+                return mid+1;
+            }
+            else if(target < arr[mid]){
+                high = mid - 1;
+            }
+            else{
+                low = mid + 1;
+            }
+        }
+        return -1;
+    }
+}
